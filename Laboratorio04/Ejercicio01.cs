20f7035482b021e2e5159d5fa613ff57d7d4a067
lfgonzami <fernandogm2000@gmail.com>
2023-04-22 16:53:33 -0600

    Resolver ejercicio 1

--- Laboratorio04/Ejercicio01.cs
+++ Laboratorio04/Ejercicio01.cs
@@ -10,7 +10,7 @@ namespace Laboratorio04
     {
         public static int Sumar(int a, int b)
         {
-            int resultado = a + b;
+            int resultado = a+b;
 
             return resultado;
         }
